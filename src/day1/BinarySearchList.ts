75a28db21afe015a7570f50ca31778c24cef6b9f
michelbeyrouty <michelbeyrouty@gmail.com>
2023-04-15 22:10:00 -0400

    [1] binary_search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,30 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
 
+    let low = 0;
+    let high = haystack.length;
+
+    while (low < high) {
+
+        const midpoint = Math.floor(low + (high - low) / 2)
+        const value = haystack[midpoint]
+
+        if (value === needle) {
+            return true
+        }
+
+        if (value > needle) {
+            high = midpoint
+        }
+
+        if (value < needle) {
+            low = midpoint + 1
+        }
+
+    }
+
+    return false
+
 }
+
+
+// complexity -> O(log(N))
