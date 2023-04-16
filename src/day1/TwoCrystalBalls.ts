ced1319f3daa85dbfbd4bf820632af4ed3cd22de
michelbeyrouty <michelbeyrouty@gmail.com>
2023-04-15 23:57:57 -0400

    [1] two_crystal_balls

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
 
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length))
+
+    let index = jumpAmount
+    for (; index < breaks.length; index += jumpAmount) {
+        if (breaks[index]) {
+            break;
+        }
+    }
+
+    index -= jumpAmount;
+    for (let j = 0; j < jumpAmount && index < breaks.length; j++, index++) {
+        if (breaks[index]) {
+            return index
+        }
+    }
+
+    return -1
 }
