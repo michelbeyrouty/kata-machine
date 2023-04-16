88e43354c1f14ce4290b73d66118562e22f65789
michelbeyrouty <michelbeyrouty@gmail.com>
2023-04-16 00:11:01 -0400

    [1] bubble_sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,11 +1,9 @@
 export default function bubble_sort(arr: number[]): void {
 
-    let temp = 0;
-
     for (let i = 0; i < arr.length; i++) {
         for (let j = 0; j < (arr.length - i - 1); j++) {
             if (arr[j] > arr[j + 1]) {
-                temp = arr[j];
+                const temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
