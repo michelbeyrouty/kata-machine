0656194d175f2077f06295065ace3e4ca0d2260b
michelbeyrouty <michelbeyrouty@gmail.com>
2023-04-20 19:58:40 -0400

    [1] Dpeth first search traversal

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,26 @@
 export default function pre_order_search(head: BinaryNode<number>): number[] {
 
+    return walk(head, [])
+
 }
+
+function walk(current: BinaryNode<number> | null, path: number[]): number[] {
+
+    if (!current) {
+        return path;
+    }
+
+    // pre-recurse
+    path.push(current.value)
+
+    // recurse
+    walk(current.left, path)
+    walk(current.right, path)
+
+    // post-recurse
+    return path
+}
+
+
+// Called DepthFirstSerach
+// O(N)
