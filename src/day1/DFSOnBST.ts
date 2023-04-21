52bf2924b76d09fc5718f948b2b57365de991d8c
michelbeyrouty <michelbeyrouty@gmail.com>
2023-04-20 21:14:07 -0400

    [1] DFSOnBST

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,22 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
 
+    return recursiveFind(head, needle)
+}
+
+
+function recursiveFind(current: BinaryNode<number> | null, needle: number): boolean {
+
+    if (current === null) {
+        return false
+    }
+
+    if (current.value == needle) {
+        return true
+    }
+
+    if (current.value <= needle) {
+        return recursiveFind(current.right, needle)
+    }
+
+    return recursiveFind(current.left, needle)
 }
