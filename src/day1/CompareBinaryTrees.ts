2bbc48030fcd8e12f294569ac270d733f9e351a0
michelbeyrouty <michelbeyrouty@gmail.com>
2023-04-20 20:30:42 -0400

    [1] CompareBinaryTrees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,22 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
 
+    return recursiveCompare(a, b)
+}
+
+
+function recursiveCompare(a: BinaryNode<number> | null | undefined, b: BinaryNode<number> | null | undefined): boolean {
+
+    if (!a === null && b === null) {
+        return true
+    }
+
+    if (a === null || b === null) {
+        return false
+    }
+
+    if (a?.value !== b?.value) {
+        return false
+    }
+
+    return recursiveCompare(a?.left, b?.left) && recursiveCompare(a?.right, b?.right)
 }
