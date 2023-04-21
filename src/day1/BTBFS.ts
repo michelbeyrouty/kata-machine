ce8efe48d97f33a2666bfc1524a0d9c67371553a
michelbeyrouty <michelbeyrouty@gmail.com>
2023-04-20 20:15:31 -0400

    [1] Breadth first search traversal

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,23 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
 
+    const q = [head];
+
+    while (q.length) {
+        if (needle === q.shift()?.value) {
+            return true
+        }
+
+        if (head.left) {
+            q.push(head.left)
+        }
+        if (head.right) {
+            q.push(head.right)
+        }
+    }
+
+    return false
 }
+
+
+// You can't do bfs in recursive because it follows a queue approach
+// In fact stack preserves shape, stack ==> recursion
